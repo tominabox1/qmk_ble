8dc0a0853121949708513ae07b5a9c6882c09729
TJ Campie <tom.campie@gmail.com>
2020-08-26 13:20:28 -0500

    latest keymap update

--- keyboards/qaz_ble/keymaps/default/config.h
+++ keyboards/qaz_ble/keymaps/default/config.h
@@ -3,5 +3,5 @@
 #define COMBO_COUNT 5
 #define COMBO_TERM 50
 
-#define TAPPING_TERM 300
+#define TAPPING_TERM 250
 #define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/qaz_ble/rev1/config.h
+++ keyboards/qaz_ble/rev1/config.h
@@ -6,13 +6,14 @@
 #define ENABLE_STARTUP_ADV_NOLIST
 #define IS_LEFT_HAND  true
 
-#define RGB_DI_PIN F7
+#define RGB_DI_PIN ((1 << 6) | 15)
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 5
+#define RGBLED_NUM 21
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #endif
+
 
 #endif /* MASTER_CONFIG_H_ */
